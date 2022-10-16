6c9a69ce6726498f7c6afe6397bf0290c30778fb
Bhanuji Malith Meegoda <49988198+BhanuMM@users.noreply.github.com>
2022-10-16 18:14:53 +0530

    service controller added

---
+++ app/Http/Controllers/ServiceContoller.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Service;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\DB;
+
+class ServiceController extends Controller
+{
+    /**
+     * Display a listing of the prducts.
+     *
+     * @return \Illuminate\Http\Response
+     */
+   
+
+    // product view
+    public function serviceviewpage(Request $request, $id)
+    {
+        // $Service = Service::where('id', '=', $id);
+        // return view('Servicequickview', compact('Service', $Service));
+
+
+        $Service = DB::table('Services')->where('id', $id)->first();
+
+        return view('Servicequickview', ['Service' => $Service]);
+    }
+}
